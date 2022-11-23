351e82691914e9eea351963ba53eb64bb0b01cc8
Vika <tViks1506@mail.ru>
2022-11-23 19:40:04 +0300

    task 1

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+    Console.Write ("число "+ a + " больше числа "+ b);
+else if ( b > a)
+    Console.Write ("число "+ b+ " больше числа "+a);
+else
+    Console.Write ("введенные числа равны между собой");
