eb886369b55794502f50726597693f24400e5ebf
Vika <tViks1506@mail.ru>
2022-11-23 20:53:12 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("введите число: ");
+int n=int.Parse(Console.ReadLine());
+if (n%2==0)
+    Console.Write("да, введенное число четное");
+else
+    Console.Write("нет, введенное число нечетное");
