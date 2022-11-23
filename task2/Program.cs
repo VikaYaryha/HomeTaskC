3513e54bb3f73647ab5752b7fc277efb6e2f9b68
Vika <tViks1506@mail.ru>
2022-11-23 20:28:24 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a=int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b=int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c=int.Parse(Console.ReadLine());
+
+if (a>=b && a>=c)
+    Console.Write("Максимальное число "+a);
+else if (b>=a && b>=c)
+    Console.Write("Максимальное число "+b);
+else
+    Console.Write("Максимальное число "+c);
+
+
