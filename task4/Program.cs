d7e761696b5bf536b3e1e34f88b308dfe7027414
Vika <tViks1506@mail.ru>
2022-11-23 21:29:29 +0300

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("введите число больше 1: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i=1; 
+if (n>1)
+{
+    while (i<=n)
+    {
+        if (i%2==0)
+            Console.Write ($"{i} ");
+            i++;
+    }
+    
+}
+else
+    Console.Write("Вы ввели число меньше 1");
