931fa7bbc37f76d8c60959251a7d17e2a582db78
Vika <tViks1506@mail.ru>
2022-11-24 15:02:40 +0300

    task2_3 zadacha 15

---
+++ home_task2_3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите числом день недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n == 6 || n == 7)
+    Console.Write("выходной день");
+else if ( n >0 && n < 6)
+    Console.Write("будний день");
+else
+    Console.Write("введено не корректное число");
