ca1554755a241872ffe8c1ab689a38a46893b5ef
Vika <tViks1506@mail.ru>
2022-11-24 12:34:05 +0300

    task2_1 zadacha 10

---
+++ home_task2_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+int n = new Random().Next(100,1000);
+Console.WriteLine($"Случайное число: {n}");
+int n2 = ( n / 10) % 10; 
+
+Console.Write(n2);
+
+
+// Или "ручной" способ указан ниже:
+
+// Console.Write("Введите трехзначное число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int n2 = ( n / 10) % 10; 
+
+// Console.Write(n2);
