05c533b4fe1c5484d915d074ef85b1ed1e341655
Vika <tViks1506@mail.ru>
2022-11-24 14:54:59 +0300

    task2_2 zadacha 13

---
+++ home_task2_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n<100) 
+    Console.Write("третьей цифры нет");
+else
+    while (n>=1000)
+    {
+        n/=10;
+        Console.Write(n%10);
+    }
+        
+    
+
