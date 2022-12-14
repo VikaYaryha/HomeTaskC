32552035eada86b20d9efa45a28bb9630a064b0e
Vika <tViks1506@mail.ru>
2022-12-14 20:46:56 +0300

    Misha

---
+++ home_task7_4_dop2/Program.cs
@@ -0,0 +1,53 @@
+﻿//Миша и негатив
+
+void InputMatrix(string [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Console.ReadLine();
+        }
+
+        Console.WriteLine();
+    }
+}
+
+
+void OutputMatrix(string [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+string[] numbers = Console.ReadLine().Split(" ");
+string [,] picture = new string [int.Parse(numbers[0]), int.Parse(numbers[1])];
+Console.WriteLine("Введите W и B исходного изображения: ");
+InputMatrix(picture);
+OutputMatrix(picture);
+string [,] pictureMisha = new string [int.Parse(numbers[0]), int.Parse(numbers[1])];
+Console.WriteLine("Введите W и B мишиного изображения: ");
+InputMatrix(pictureMisha);
+OutputMatrix(pictureMisha);
+
+int count = 0;
+for (int i = 0; i < picture.GetLength(0); i++)
+    {
+        for (int j = 0; j < picture.GetLength(1); j++)
+        {
+            if (picture [i,j] != pictureMisha [i,j])
+            count++;
+        }
+
+    }
+
+Console.WriteLine($"Количество неправильно сформированных пикселей: {count}");
