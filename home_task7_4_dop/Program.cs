8308b56e9c5f359ac15fdd2b76bc13b0e13f1de7
Vika <tViks1506@mail.ru>
2022-12-14 17:37:01 +0300

    transponirovanie

---
+++ home_task7_4_dop/Program.cs
@@ -0,0 +1,46 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 101);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void OutputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+string[] numbers = Console.ReadLine().Split(" ");
+int[,] matrix = new int[int.Parse(numbers[0]), int.Parse(numbers[1])];
+Console.WriteLine("Исходный массив: ");
+InputMatrix(matrix);
+int[,] matrixNew = new int[int.Parse(numbers[0]), int.Parse(numbers[1])];
+
+for (int i = 0; i < matrixNew.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixNew.GetLength(1); j++)
+        {
+           matrixNew[(matrixNew.GetLength(0) - 1 - i),j] = matrix [i,j];
+        }
+        
+    }
+Console.WriteLine("Результат: ");
+OutputMatrix(matrixNew);
