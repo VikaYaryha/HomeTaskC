a9aed0bdbd0d8c1685efd42d9f92d6827a064db8
Vika <tViks1506@mail.ru>
2022-12-16 12:57:52 +0300

    zadacha 60

---
+++ home_task_8_4/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив,
+// добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+void InputMatrixKvadro(int[ , , ] matrix, int n)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = ++n;
+                Console.Write($"{matrix[i, j, k]}({i}, {j}, {k})  ");
+            }
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] coord = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+while (coord[0]*coord[1]*coord[2] > 100)
+{
+    Console.WriteLine("Введите меньший массив");
+    coord = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+}
+int[, ,] matrix = new int[coord[0], coord[1], coord[2]];
+int n = 10;
+InputMatrixKvadro(matrix, n);
+
+
